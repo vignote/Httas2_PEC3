a3aea8057c47e6a75eb47be15e7509268599ff17
Alejandro Rodriguez Vignote <alejandro.vignote@gmail.com>
2021-05-18 19:57:09 +0200

    after setting dev suite

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,21 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
+
+//Import lobster font
+import "@fontsource/lobster";
+//Import oswald font
+import "@fontsource/oswald";
