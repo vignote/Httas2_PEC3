defdeabf8b95d820bcde53357abd1860ae0401b7
Alejandro Rodriguez Vignote <alejandro.vignote@gmail.com>
2021-05-19 08:04:36 +0200

    first two pages with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      transform: ["hover"],
+      letterSpacing: ["hover", "focus"],
+      fontSize: ["hover", "focus"],
+      fontFamily: {
+        heading: [
+          "Lobster",
+          "Lobster",
+          "Roboto",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+        sans: ["Oswald", "Georgia", "sans-serif"],
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      yellow: "#e3b119",
+      lightYellow: "#dfd97b",
+      darkYellow: "#c49c24;",
+    },
   },
   variants: {
     extend: {},
